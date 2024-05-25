3039822ab7affc054217db476c116ad015156082
papel <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2024-05-25 05:00:31 +0200

    Introduced coarse problem

--- python/demo/demo_twogrid.py
+++ python/demo/demo_twogrid.py
@@ -11,24 +11,30 @@ from ufl import ds, dx, grad, inner
 
 comm = MPI.COMM_WORLD
 
-msh_fine = mesh.create_rectangle(
+create_mesh = lambda n: mesh.create_rectangle(
     comm=comm,
     points=((0.0, 0.0), (1.0, 1.0)),
-    n=(16, 16),
+    n=(n, n),
     cell_type=mesh.CellType.triangle,
 )
 
+msh_coarse = create_mesh(8)
+msh_fine = create_mesh(16)
+
+V_coarse = fem.functionspace(msh_coarse, ("Lagrange", 1))
 V_fine = fem.functionspace(msh_fine, ("Lagrange", 1))
 
-facets_fine = mesh.locate_entities_boundary(
-    msh_fine,
-    dim=(msh_fine.topology.dim - 1),
-    marker=lambda x: np.isclose(x[0], 0.0) | np.isclose(x[0], 1.0),
-)
+def create_bc(V):
+    facets_fine = mesh.locate_entities_boundary(
+        V.mesh,
+        dim=(V.mesh.topology.dim - 1),
+        marker=lambda x: np.isclose(x[0], 0.0) | np.isclose(x[0], 1.0),
+    )
 
-dofs_fine = fem.locate_dofs_topological(V=V_fine, entity_dim=1, entities=facets_fine)
+    dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets_fine)
+    return fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
 
-bc_fine = fem.dirichletbc(value=ScalarType(0), dofs=dofs_fine, V=V_fine)
+bc_fine = create_bc(V_fine)
 
 def variational_problem(V):
     u = ufl.TrialFunction(V)
@@ -40,10 +46,19 @@ def variational_problem(V):
     L = inner(f, v) * dx + inner(g, v) * ds
     return a, L
 
+a_coarse, L_coarse = variational_problem(V_coarse)
 a_fine, L_fine = variational_problem(V_fine)
-problem = LinearProblem(a_fine, L_fine, bcs=[bc_fine], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
-uh = problem.solve()
 
-with io.XDMFFile(comm, "out_twogrid/poisson.xdmf", "w") as file:
+problem_fine = LinearProblem(a_fine, L_fine, bcs=[bc_fine], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+u_fine = problem_fine.solve()
+
+with io.XDMFFile(comm, "out_twogrid/fine.xdmf", "w") as file:
     file.write_mesh(msh_fine)
-    file.write_function(uh)
+    file.write_function(u_fine)
+
+problem_coarse = LinearProblem(a_fine, L_fine, bcs=[bc_fine], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+u_coarse = problem_coarse.solve()
+
+with io.XDMFFile(comm, "out_twogrid/coarse.xdmf", "w") as file:
+    file.write_mesh(msh_coarse)
+    file.write_function(u_coarse)
