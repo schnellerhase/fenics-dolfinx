6b95df091f73bb2bb24dc0a48b864f49d67dfdd4
papel <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2024-06-03 11:21:28 +0200

    Working prolongation

--- python/demo/demo_twogrid.py
+++ python/demo/demo_twogrid.py
@@ -52,10 +52,6 @@ a_fine, L_fine = variational_problem(V_fine)
 problem_fine = LinearProblem(a_fine, L_fine, bcs=[bc_fine], petsc_options= {"ksp_type": "chebyshev", "pc_type": "none", "ksp_max_it": 10, "ksp_monitor": ""})
 u_fine = problem_fine.solve()
 
-with io.XDMFFile(comm, "out_twogrid/fine.xdmf", "w") as file:
-    file.write_mesh(msh_fine)
-    file.write_function(u_fine)
-
 # implement the restriction of fine problem to coarse via non matching mesh interpolation, i.e. interpolate residual of fine problem on coarse mesh
 A_fine = problem_fine.A
 b_fine = problem_fine.b
@@ -67,7 +63,7 @@ A_fine.multAdd(u_fine.vector, b_fine, residual_fine.x.petsc_vec)
 
 print(f"Fine residual: {residual_fine.vector.norm():e}")
 
-# Petform non matching cell interpolation
+# Petform restriction by non matching cell interpolation
 # checkout test_interpolation.py for this in further detail
 coarse_mesh_cell_map = msh_coarse.topology.index_map(msh_coarse.topology.dim)
 num_cells_on_proc = coarse_mesh_cell_map.size_local + coarse_mesh_cell_map.num_ghosts
@@ -77,11 +73,42 @@ interpolation_data = fem.create_interpolation_data(V_coarse, V_fine, cells, padd
 residual_coarse = fem.Function(V_coarse)
 residual_coarse.interpolate_nonmatching(residual_fine, cells, interpolation_data=interpolation_data)
 
-print(f"Coarse residual: {residual_coarse.vector.norm():e}")
+print(f"Coarse residual (pre solve): {residual_coarse.vector.norm():e}")
 
 problem_coarse = LinearProblem(a_fine, L_fine, bcs=[bc_fine], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
 u_coarse = problem_coarse.solve()
 
+print(f"Coarse residual (post solve): {u_coarse.vector.norm():e}")
+
 with io.XDMFFile(comm, "out_twogrid/coarse.xdmf", "w") as file:
     file.write_mesh(msh_coarse)
     file.write_function(u_coarse)
+
+# Petform prolongation by non matching cell interpolation
+fine_mesh_cell_map = msh_fine.topology.index_map(msh_fine.topology.dim)
+num_cells_on_proc = fine_mesh_cell_map.size_local + fine_mesh_cell_map.num_ghosts
+cells = np.arange(num_cells_on_proc, dtype=np.int32)
+interpolation_data = fem.create_interpolation_data(V_fine, V_coarse, cells, padding=1e-14)
+
+residual_fine.interpolate_nonmatching(residual_coarse, cells, interpolation_data=interpolation_data)
+
+# Update fine solution: u_fine -= residual_fine
+u_fine.vector.axpy(-1, residual_fine.vector)
+
+# recompute residual
+A_fine.multAdd(u_fine.vector, b_fine, residual_fine.x.petsc_vec)
+print(f"Fine residual (pre smoothing): {residual_fine.vector.norm():e}")
+
+# smoothing
+# problem_fine.solve()
+problem_fine = LinearProblem(a_fine, L_fine, bcs=[bc_fine], u=u_fine, petsc_options= {"ksp_type": "chebyshev", "pc_type": "none", "ksp_max_it": 10, "ksp_monitor": ""})
+u_fine = problem_fine.solve()
+A_fine = problem_fine.A
+b_fine = problem_fine.b
+# recompute residual
+A_fine.multAdd(u_fine.vector, b_fine, residual_fine.x.petsc_vec)
+print(f"Fine residual (post smoothing): {residual_fine.vector.norm():e}")
+
+with io.XDMFFile(comm, "out_twogrid/fine.xdmf", "w") as file:
+    file.write_mesh(msh_fine)
+    file.write_function(u_fine)
