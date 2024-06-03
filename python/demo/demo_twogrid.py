9e5149a985de6be36aa60aa5bfcbe6e594fc435b
papel <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2024-06-03 10:57:33 +0200

    Working restriction and coarse (exact) solve

--- python/demo/demo_twogrid.py
+++ python/demo/demo_twogrid.py
@@ -5,7 +5,7 @@ from petsc4py.PETSc import ScalarType
 import numpy as np
 
 import ufl
-from dolfinx import fem, io, mesh, plot
+from dolfinx import fem, io, mesh, plot, la
 from dolfinx.fem.petsc import LinearProblem
 from ufl import ds, dx, grad, inner
 
@@ -55,6 +55,29 @@ u_fine = problem_fine.solve()
 with io.XDMFFile(comm, "out_twogrid/fine.xdmf", "w") as file:
     file.write_mesh(msh_fine)
     file.write_function(u_fine)
+
+# implement the restriction of fine problem to coarse via non matching mesh interpolation, i.e. interpolate residual of fine problem on coarse mesh
+A_fine = problem_fine.A
+b_fine = problem_fine.b
+
+# residual_fine = A_fine u_fine - b_fine
+residual_fine = fem.Function(V_fine)
+b_fine *= -1
+A_fine.multAdd(u_fine.vector, b_fine, residual_fine.x.petsc_vec)
+
+print(f"Fine residual: {residual_fine.vector.norm():e}")
+
+# Petform non matching cell interpolation
+# checkout test_interpolation.py for this in further detail
+coarse_mesh_cell_map = msh_coarse.topology.index_map(msh_coarse.topology.dim)
+num_cells_on_proc = coarse_mesh_cell_map.size_local + coarse_mesh_cell_map.num_ghosts
+cells = np.arange(num_cells_on_proc, dtype=np.int32)
+interpolation_data = fem.create_interpolation_data(V_coarse, V_fine, cells, padding=1e-14)
+
+residual_coarse = fem.Function(V_coarse)
+residual_coarse.interpolate_nonmatching(residual_fine, cells, interpolation_data=interpolation_data)
+
+print(f"Coarse residual: {residual_coarse.vector.norm():e}")
 
 problem_coarse = LinearProblem(a_fine, L_fine, bcs=[bc_fine], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
 u_coarse = problem_coarse.solve()
