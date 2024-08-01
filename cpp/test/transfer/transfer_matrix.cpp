b21cc502edc2ba1e827da54160dfbd4fd5f2f7cf
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-08-01 15:22:49 +0200

    Introduce transfer matrix operation

--- cpp/test/transfer/transfer_matrix.cpp
+++ cpp/test/transfer/transfer_matrix.cpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <memory>
 #include <optional>
+#include <vector>
 
 #include <mpi.h>
 
@@ -17,6 +18,8 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <catch2/matchers/catch_matchers_range_equals.hpp>
 
+#include <dolfinx/common/IndexMap.h>
+#include <dolfinx/common/MPI.h>
 #include <dolfinx/fem/FunctionSpace.h>
 #include <dolfinx/fem/utils.h>
 #include <dolfinx/la/MatrixCSR.h>
@@ -45,9 +48,8 @@ TEMPLATE_TEST_CASE("Transfer Matrix 1D", "[transfer_matrix]",
       = std::make_shared<mesh::Mesh<T>>(dolfinx::mesh::create_interval<T>(
           MPI_COMM_SELF, 2, {0.0, 1.0}, mesh::GhostMode::none));
 
-  auto [mesh_fine, parent_cell, parent_facet] = refinement::refine(
-      *mesh_coarse, std::nullopt, &refinement::maintain_coarse_partitioner, mesh::GhostMode::none,
-      refinement::Option::parent_cell);
+  auto [mesh_fine, parent_cell, parent_facet]
+      = refinement::refine(*mesh_coarse, std::nullopt);
 
   auto element = basix::create_element<T>(
       basix::element::family::P, basix::cell::type::interval, 1,
@@ -63,10 +65,18 @@ TEMPLATE_TEST_CASE("Transfer Matrix 1D", "[transfer_matrix]",
   mesh_fine.topology()->create_connectivity(1, 0);
   mesh_fine.topology()->create_connectivity(0, 1);
 
-  std::vector<std::int64_t> from_to_map{0, 2, 4}; // TODO: general computation!
+  std::vector<std::int64_t> inclusion_map
+      = transfer::inclusion_mapping(*mesh_coarse, mesh_fine);
 
-  la::MatrixCSR<T> transfer_matrix = transfer::create_transfer_matrix<T>(
-      *V_coarse, *V_fine, from_to_map, weight<T>);
+  CHECK_THAT(inclusion_map, RangeEquals(std::vector<std::int64_t>{0, 2, 4}));
+
+  la::SparsityPattern sp
+      = transfer::create_sparsity_pattern(*V_coarse, *V_fine, inclusion_map);
+
+  la::MatrixCSR<T> transfer_matrix(std::move(sp), la::BlockMode::compact);
+  transfer::assemble_transfer_matrix<T>(transfer_matrix.mat_set_values(),
+                                        *V_coarse, *V_fine, inclusion_map,
+                                        weight<T>);
 
   std::vector<T> expected{1.0, .5, 0, 0, 0, 0, .5, 1, .5, 0, 0, 0, 0, .5, 1};
   CHECK_THAT(transfer_matrix.to_dense(),
@@ -83,7 +93,7 @@ TEMPLATE_TEST_CASE("Transfer Matrix 1D (parallel)", "[transfer_matrix]",
   int rank = dolfinx::MPI::rank(MPI_COMM_WORLD);
 
   // TODO: see https://github.com/FEniCS/dolfinx/issues/3358
-  // auto part = mesh::create_cell_partitioner(mesh::GhostMode::shared_facet);
+  //   auto part = mesh::create_cell_partitioner(mesh::GhostMode::shared_facet);
   mesh::CellPartitionFunction part
       = [&](MPI_Comm /* comm */, int /* nparts */,
             const std::vector<mesh::CellType>& /* cell_types */,
@@ -106,9 +116,40 @@ TEMPLATE_TEST_CASE("Transfer Matrix 1D (parallel)", "[transfer_matrix]",
       mesh::create_interval<T>(MPI_COMM_WORLD, 5 * comm_size - 1, {0., 1.},
                                mesh::GhostMode::shared_facet, part));
 
+  // TODO: see https://github.com/FEniCS/dolfinx/issues/3358
+  //   auto part_refine
+  //       = mesh::create_cell_partitioner(mesh::GhostMode::shared_facet);
+  mesh::CellPartitionFunction part_refine
+      = [&](MPI_Comm comm, int /* nparts */,
+            const std::vector<mesh::CellType>& /* cell_types */,
+            const std::vector<std::span<const std::int64_t>>& /* cells */)
+  {
+    std::vector<std::vector<std::int32_t>> data;
+    if (dolfinx::MPI::size(comm) == 1)
+      data = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}};
+    else if (dolfinx::MPI::size(comm) == 2)
+    {
+      if (rank == 0)
+        data = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0, 1}, {1, 0}};
+      else
+        data = {{1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}};
+    }
+    else if (dolfinx::MPI::size(comm) == 3)
+    {
+      if (rank == 0)
+        data = {{2, 0}, {0, 2}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}};
+      else if (rank == 1)
+        data = {{1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1, 2}};
+      else
+        data = {{2, 1}, {2}, {2}, {2}, {2}, {2}, {2}, {2}};
+    }
+    else
+      FAIL("Test only supports <= 3 processes");
+    return graph::AdjacencyList<std::int32_t>(std::move(data));
+  };
+
   auto [mesh_fine, parent_cell, parent_facet] = refinement::refine(
-      *mesh_coarse, std::nullopt, part, mesh::GhostMode::shared_facet,
-      refinement::Option::none);
+      *mesh_coarse, std::nullopt, part_refine, refinement::Option::none);
 
   auto element = basix::create_element<T>(
       basix::element::family::P, basix::cell::type::interval, 1,
@@ -124,22 +165,30 @@ TEMPLATE_TEST_CASE("Transfer Matrix 1D (parallel)", "[transfer_matrix]",
   mesh_fine.topology()->create_connectivity(1, 0);
   mesh_fine.topology()->create_connectivity(0, 1);
 
-  std::vector<int64_t> from_to_map;
+  std::vector<int64_t> inclusion_map;
   switch (comm_size)
   {
   case 1:
-    from_to_map = {0, 2, 4, 6, 8};
+    inclusion_map = {0, 2, 4, 6, 8};
     break;
   case 2:
-    from_to_map = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
+    inclusion_map = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
     break;
   case 3:
-    from_to_map = {0, 2, 4, 6, 8, 9, 11, 13, 15, 17, 19, 28, 26, 24, 22};
+    inclusion_map = {0, 2, 4, 6, 8, 9, 11, 13, 15, 17, 19, 27, 25, 23, 20};
     break;
   }
 
-  la::MatrixCSR<T> transfer_matrix = transfer::create_transfer_matrix(
-      *V_coarse, *V_fine, from_to_map, weight<T>);
+  CHECK_THAT(transfer::inclusion_mapping(*mesh_coarse, mesh_fine),
+             RangeEquals(inclusion_map));
+
+  la::SparsityPattern sp
+      = transfer::create_sparsity_pattern(*V_coarse, *V_fine, inclusion_map);
+
+  la::MatrixCSR<T> transfer_matrix(std::move(sp), la::BlockMode::compact);
+  transfer::assemble_transfer_matrix<T>(transfer_matrix.mat_set_values(),
+                                        *V_coarse, *V_fine, inclusion_map,
+                                        weight<T>);
 
   std::array<std::vector<T>, 3> expected;
   if (comm_size == 1)
@@ -198,13 +247,13 @@ TEMPLATE_TEST_CASE("Transfer Matrix 1D (parallel)", "[transfer_matrix]",
         /* row_6 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     };
     expected[2] = {
-        /* row_0 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0,
-        /* row_1 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0,
-        /* row_2 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0, 0.0, 0.0,
-        /* row_3 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+        /* row_0 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5,
+        /* row_1 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0, 0.0,
+        /* row_2 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0,
+        /* row_3 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         /* row_4 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         /* row_5 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        /* row_6 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        /* row_6 */ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
     };
     // clang-format on
   }
@@ -227,9 +276,8 @@ TEMPLATE_TEST_CASE("Transfer Matrix 2D", "[transfer_matrix]", double)
           MPI_COMM_SELF, {{{0, 0}, {1, 1}}}, {1, 1}, mesh::CellType::triangle));
   mesh_coarse->topology()->create_entities(1);
 
-  auto [mesh_fine, parent_cell, parent_facet] = refinement::refine(
-      *mesh_coarse, std::nullopt, &refinement::maintain_coarse_partitioner, mesh::GhostMode::none,
-      refinement::Option::parent_cell);
+  auto [mesh_fine, parent_cell, parent_facet]
+      = refinement::refine(*mesh_coarse, std::nullopt);
 
   auto element = basix::create_element<T>(
       basix::element::family::P, basix::cell::type::triangle, 1,
@@ -245,12 +293,17 @@ TEMPLATE_TEST_CASE("Transfer Matrix 2D", "[transfer_matrix]", double)
   mesh_fine.topology()->create_connectivity(1, 0);
   mesh_fine.topology()->create_connectivity(0, 1);
 
-  std::vector<std::int64_t> from_to_map{4, 1, 5,
-                                        8}; // TODO: general computation!
+  std::vector<std::int64_t> inclusion_map
+      = transfer::inclusion_mapping(*mesh_coarse, mesh_fine);
+  CHECK_THAT(inclusion_map, RangeEquals(std::vector<std::int64_t>{4, 1, 5, 8}));
 
-  la::MatrixCSR<T> transfer_matrix = transfer::create_transfer_matrix(
-      *V_coarse, *V_fine, from_to_map, weight<T>);
-
+  la::SparsityPattern sp
+      = transfer::create_sparsity_pattern(*V_coarse, *V_fine, inclusion_map);
+  la::MatrixCSR<T> transfer_matrix(std::move(sp), la::BlockMode::compact);
+  transfer::assemble_transfer_matrix<T>(transfer_matrix.mat_set_values(),
+                                        *V_coarse, *V_fine, inclusion_map,
+                                        weight<T>);
+  transfer_matrix.scatter_rev();
   std::vector<T> expected{0.5, 0.0, 0.5, 0.0, 1.0, 0.0, 0.5, 0.0, 0.0,
                           0.5, 1.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
                           0.5, 0.0, 0.0, 0.5, 0.0, 1.0, 0.0, 0.5, 0.0,
@@ -269,9 +322,8 @@ TEMPLATE_TEST_CASE("Transfer Matrix 3D", "[transfer_matrix]", double)
                                    {1, 1, 1}, mesh::CellType::tetrahedron));
   mesh_coarse->topology()->create_entities(1);
 
-  auto [mesh_fine, parent_cell, parent_facet] = refinement::refine(
-      *mesh_coarse, std::nullopt, &refinement::maintain_coarse_partitioner, mesh::GhostMode::none,
-      refinement::Option::parent_cell);
+  auto [mesh_fine, parent_cell, parent_facet]
+      = refinement::refine(*mesh_coarse, std::nullopt);
 
   auto element = basix::create_element<T>(
       basix::element::family::P, basix::cell::type::tetrahedron, 1,
@@ -287,11 +339,17 @@ TEMPLATE_TEST_CASE("Transfer Matrix 3D", "[transfer_matrix]", double)
   mesh_fine.topology()->create_connectivity(1, 0);
   mesh_fine.topology()->create_connectivity(0, 1);
 
-  std::vector<std::int64_t> from_to_map{
-      0, 6, 15, 25, 17, 9, 11, 22}; // TODO: general computation!
+  std::vector<std::int64_t> inclusion_map
+      = transfer::inclusion_mapping(*mesh_coarse, mesh_fine);
+  CHECK_THAT(inclusion_map, RangeEquals(std::vector<std::int64_t>{
+                                0, 6, 15, 25, 17, 9, 11, 22}));
 
-  la::MatrixCSR<T> transfer_matrix = transfer::create_transfer_matrix(
-      *V_coarse, *V_fine, from_to_map, weight<T>);
+  la::SparsityPattern sp
+      = transfer::create_sparsity_pattern(*V_coarse, *V_fine, inclusion_map);
+  la::MatrixCSR<T> transfer_matrix(std::move(sp), la::BlockMode::compact);
+  transfer::assemble_transfer_matrix<T>(transfer_matrix.mat_set_values(),
+                                        *V_coarse, *V_fine, inclusion_map,
+                                        weight<T>);
 
   std::vector<T> expected{
       1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
