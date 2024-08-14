6d529c46abd5ee98e15241267bc1220c64e6a124
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-08-14 15:34:35 +0200

    Add 3D test case

--- cpp/test/transfer/transfer_matrix.cpp
+++ cpp/test/transfer/transfer_matrix.cpp
@@ -85,7 +85,6 @@ TEST_CASE("Transfer Matrix 2D", "[transfer_matrix]")
       *mesh_coarse, std::nullopt, redistribute, mesh::GhostMode::none,
       refinement::Option::parent_cell);
 
-
   auto element = basix::create_element<double>(
       basix::element::family::P, basix::cell::type::triangle, 1,
       basix::element::lagrange_variant::unset,
@@ -102,13 +101,70 @@ TEST_CASE("Transfer Matrix 2D", "[transfer_matrix]")
 
   std::vector<int32_t> from_to_map{4, 1, 5, 8}; // TODO: general computation!
 
-    la::MatrixCSR<double> transfer_matrix
+  la::MatrixCSR<double> transfer_matrix
       = transfer::create_transfer_matrix(*V_coarse, *V_fine, from_to_map);
 
   std::vector<double> expected{0.5, 0.0, 0.5, 0.0, 1.0, 0.0, 0.5, 0.0, 0.0,
                                0.5, 1.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
                                0.5, 0.0, 0.0, 0.5, 0.0, 1.0, 0.0, 0.5, 0.0,
                                0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 1.0};
+  CHECK_THAT(transfer_matrix.to_dense(),
+             RangeEquals(expected, [](auto a, auto b)
+                         { return std::abs(a - b) <= EPS<T>; }));
+}
+
+TEST_CASE("Transfer Matrix 3D", "[transfer_matrix]")
+{
+  using T = double;
+
+  auto part = mesh::create_cell_partitioner(mesh::GhostMode::shared_vertex);
+  auto mesh_coarse
+      = std::make_shared<mesh::Mesh<double>>(dolfinx::mesh::create_box<double>(
+          MPI_COMM_SELF, {{{0, 0, 0}, {1, 1, 1}}}, {1, 1, 1},
+          mesh::CellType::tetrahedron));
+  mesh_coarse->topology()->create_entities(1);
+
+  bool redistribute = false;
+  auto [mesh_fine, parent_cell, parent_facet] = refinement::refine(
+      *mesh_coarse, std::nullopt, redistribute, mesh::GhostMode::none,
+      refinement::Option::parent_cell);
+
+  auto element = basix::create_element<double>(
+      basix::element::family::P, basix::cell::type::tetrahedron, 1,
+      basix::element::lagrange_variant::unset,
+      basix::element::dpc_variant::unset, false);
+
+  auto V_coarse = std::make_shared<fem::FunctionSpace<double>>(
+      fem::create_functionspace<double>(mesh_coarse, element, {}));
+  auto V_fine = std::make_shared<fem::FunctionSpace<double>>(
+      fem::create_functionspace<double>(
+          std::make_shared<mesh::Mesh<double>>(mesh_fine), element, {}));
+
+  mesh_fine.topology()->create_connectivity(1, 0);
+  mesh_fine.topology()->create_connectivity(0, 1);
+
+  std::vector<int32_t> from_to_map{0,  6, 15, 25,
+                                   17, 9, 11, 22}; // TODO: general computation!
+
+  la::MatrixCSR<double> transfer_matrix
+      = transfer::create_transfer_matrix(*V_coarse, *V_fine, from_to_map);
+
+  std::vector<double> expected{
+      1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5,
+      0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 0.5, 0.0,
+      0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5, 0.0, 0.5,
+      0.5, 1.0, 0.5, 0.0, 0.5, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.5, 0.0, 0.5, 0.0, 0.5, 1.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 0.0, 0.0,
+      0.0, 0.5, 0.5, 0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 1.0, 0.0, 0.0, 0.0, 0.5, 0.0,
+      0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0,
+      0.0, 0.5, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0,
+      0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5,
+      0.5, 1.0, 0.0, 0.0, 0.0, 0.5};
   CHECK_THAT(transfer_matrix.to_dense(),
              RangeEquals(expected, [](auto a, auto b)
                          { return std::abs(a - b) <= EPS<T>; }));
