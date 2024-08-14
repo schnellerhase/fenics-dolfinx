42d9ae0fe91787b230824c69d1f8eb3df2a4bffa
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-08-14 12:45:10 +0200

    Finish rectangle test

--- cpp/test/mesh/rectangle.cpp
+++ cpp/test/mesh/rectangle.cpp
@@ -87,8 +87,6 @@ TEMPLATE_TEST_CASE("Rectangle quadrilateral mesh",
                                        /* e_3 */ {2, 3}});
 }
 
-
-
 TEMPLATE_TEST_CASE("Rectangle triangle mesh (right)",
                    "[mesh][rectangle][triangle][right]", float, double)
 {
@@ -185,4 +183,55 @@ TEMPLATE_TEST_CASE("Rectangle triangle mesh (left)",
                                        /* e_4 */ {2, 3}});
 }
 
-// TODO: crossed diagonal type
+TEMPLATE_TEST_CASE("Rectangle triangle mesh (crossed)",
+                   "[mesh][rectangle][triangle][crossed]", float, double)
+{
+  using T = TestType;
+
+  if (dolfinx::MPI::size(MPI_COMM_WORLD) > 1)
+    return;
+
+  const std::array<double, 2> lower = {0, 0};
+  const std::array<double, 2> upper = {1, 1};
+
+  mesh::Mesh<T> mesh = dolfinx::mesh::create_rectangle<T>(
+      MPI_COMM_SELF, {lower, upper}, {1, 1}, mesh::CellType::triangle,
+      mesh::DiagonalType::crossed);
+
+  // vertex layout:
+  // 3---4
+  // |\ /|
+  // | 2 |
+  // |/ \|
+  // 0---1
+  std::vector<T> expected_x = {
+      /* v_0 */ 0,  0,  0,
+      /* v_1 */ 1,  0,  0,
+      /* v_2 */ .5, .5, 0,
+      /* v_3 */ 0,  1,  0,
+      /* v_4 */ 1,  1,  0,
+  };
+
+  CHECK_THAT(mesh.geometry().x(),
+             RangeEquals(expected_x, [](auto a, auto b)
+                         { return std::abs(a - b) <= EPS<T>; }));
+
+  // edge layout:
+  // x-7-x
+  // |5 6|
+  // 2 x 4
+  // |1 3|
+  // x-0-x
+  mesh.topology()->create_connectivity(1, 0);
+  auto e_to_v = mesh.topology()->connectivity(1, 0);
+  REQUIRE(e_to_v);
+
+  CHECK_adjacency_list_equal(*e_to_v, {/* e_0 */ {0, 1},
+                                       /* e_1 */ {0, 2},
+                                       /* e_2 */ {0, 3},
+                                       /* e_3 */ {1, 2},
+                                       /* e_4 */ {1, 4},
+                                       /* e_5 */ {2, 3},
+                                       /* e_6 */ {2, 4},
+                                       /* e_7 */ {3, 4}});
+}
