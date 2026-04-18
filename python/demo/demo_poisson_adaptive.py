045f3a2d613811970505c2419aece2ab3c7ce898
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2026-04-18 20:43:34 +0200

    A first solve, estimate, mark, refine loop

--- python/demo/demo_poisson_adaptive.py
+++ python/demo/demo_poisson_adaptive.py
@@ -10,6 +10,8 @@
 
 # # Adaptive FEM for the Poisson equation
 #
+# Copyright (C) 2026 Maximilian Brodbeck and Paul T. Kühner
+#
 # This demo illustrates how to:
 #
 # - Use residual-type error estimators
@@ -22,8 +24,8 @@
 # * {download}`Python script <./demo_poisson_adaptive.py>`
 # * {download}`Jupyter notebook <./demo_poisson_adaptive.ipynb>`
 # ```
-# ## TODO
 #
+# ## TODO
 # ...
 #
 # +
@@ -35,6 +37,47 @@ from petsc4py.PETSc import ScalarType  # type: ignore
 import numpy as np
 
 import ufl
-from dolfinx import fem, io, mesh, plot
+from dolfinx import fem, io, mesh
 from dolfinx.fem.petsc import LinearProblem
 
+comm = MPI.COMM_WORLD
+
+meshes = []
+meshes.append(mesh.create_unit_square(comm, n := 10, n, dtype=ScalarType))
+
+tdim = meshes[0].topology.dim
+
+for it in range(max_it := 3):
+    print(f"AFEM it. {it}:")
+
+    msh = meshes[it]
+
+    V = fem.functionspace(msh, ("CG", 1))
+    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+    a = ufl.inner(u, v) * ufl.dx
+    L = ufl.inner(-6, v) * ufl.dx
+
+    print(" SOLVE:", end="")
+    problem = LinearProblem(a, L, petsc_options_prefix="solver")
+    problem.solve()
+    print(" TODO")
+
+    print(" ESTIMATE:")
+    im_cell = msh.topology.index_map(tdim)
+    marker = np.random.default_rng(0).random(im_cell.size_local + im_cell.num_ghosts)
+
+    print(" MARK: ", end="")
+    marked_cells = np.argwhere(
+        marker >= (theta := 0.5) * comm.allreduce(np.max(marker), MPI.MAX)
+    ).flatten()
+
+    msh.topology.create_entities(1)
+    marked_edges = mesh.compute_incident_entities(msh.topology, marked_cells, tdim, 1)
+    print(f"{marked_cells.size} cells ({marked_edges.size} edges) marked for refinement")
+
+    print(" REFINE:")
+    msh_refined, _, _ = mesh.refine(msh, marked_edges)
+    meshes.append(msh_refined)
+
+    with io.XDMFFile(comm, f"output/it_{it}.xdmf", "w") as file:
+        file.write_mesh(msh)
